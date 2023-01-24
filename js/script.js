0c299ac367b0a1cf83b9c401b03e99fd3b363f0a
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-24 16:26:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
+});
+$(".menu-collapsed").click(function() {
+  $(".logo").toggleClass("position-fixed");
+});
+$(".menu-collapsed").click(function() {
+  $(".header__menu_button").toggleClass("d-none");
+});
+$(".menu-collapsed").click(function() {
+  $(".bar").toggleClass("position-fixed");
+});
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
+
+
+
